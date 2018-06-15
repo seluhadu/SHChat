3673601acff3f9ed4d8f4e45eabc6d5bb76048d5
Selu Hadu Two <34612461+SeluHaduTwo@users.noreply.github.com>
2018-06-15 09:44:25 -0400

    First Commi!

--- app/src/main/java/com/seluhadu/shchat/fragments/HomeFragment.java
+++ app/src/main/java/com/seluhadu/shchat/fragments/HomeFragment.java
@@ -8,10 +8,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.seluhadu.shchat.R;
+
 public class HomeFragment extends Fragment {
+    private FirebaseAuth mFIreBaseAuth;
+    private FirebaseFirestore mFireBaseFireStore;
+    private StorageReference mStorageReference;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_home, container, false);
+        mFIreBaseAuth = FirebaseAuth.getInstance();
+        mFireBaseFireStore = FirebaseFirestore.getInstance();
+        mStorageReference = FirebaseStorage.getInstance().getReference();
+        
+
+        return rootView;
     }
 }
